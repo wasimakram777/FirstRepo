be7a82d6122fcfa126ab2bd1356bfb667da2047b
Wasim Akram <wasim.akram@mercedes-benz.com>
2024-08-28 17:08:10 +0200

    added add.c

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {    
+    int number2,number,1 number2, sum;
+    ring the chnages in branch1
+    
+    printf("Enter two integer: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
+
